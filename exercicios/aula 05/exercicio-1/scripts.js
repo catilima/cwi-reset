48ca8c82ff14d8228b10e0acd69bc10ffacf13a5
Cati Lima <catiucia.lima@gmail.com>
2021-08-18 12:33:34 -0300

    Organização de pastas e exercicios

---
+++ exercicios/aula 05/exercicio-1/scripts.js
@@ -0,0 +1,6 @@
+let nomeDoFulano = "Cati"
+let nomeDoBeltrano = "Vagner"
+
+mesmoNome = (nomeDoBeltrano == nomeDoFulano) 
+
+console.log (mesmoNome);
---
+++ exercicios/aula 05/exercicio-3/scripts.js
@@ -0,0 +1,6 @@
+let valorBoleto = 1000
+let taxajuros = 10
+let valorJuros = valorBoleto * (taxajuros / 100)
+let valorComJuros = valorBoleto + valorJuros
+
+console.log (valorComJuros);
