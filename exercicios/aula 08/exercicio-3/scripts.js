f3f1bccd534dd49e6ef51d2477999a5ee8788b5e
Cati Lima <catiucia.lima@gmail.com>
2021-08-22 21:03:50 -0300

    aula 8

---
+++ exercicios/aula 08/exercicio-3/scripts.js
@@ -0,0 +1,23 @@
+let nomesComuns = ["Miguel", "Laura", "Lucas", "Beatriz", "Guilherme", "Maria", "Gabriel", "Ana", "Arthur", "Júlia",
+    "Enzo", "Alice", "Rafael", "Mariana", "João", "Larissa", "Gustavo", "Maria Eduarda", "Pedro", "Sofia",
+    "Bernardo", "Isabela", "Matheus", "Helena", "Davi", "Camila", "Heitor", "Lara", "Henrique", "Valentina",
+    "Bruno", "Letícia", "Samuel", "Luana", "Felipe", "Amanda", "Lorenzo", "Yasmin", "Benjamin", "Sophia",
+    "Vinícius", "Rebeca", "Rodrigo", "Juliana", "Eduardo", "Bruna", "Diego", "Cecília", "Antônio", "Fernanda",
+    "Leonardo", "Isadora", "Noah", "Lorena", "Nícolas", "Lívia", "Daniel", "Manuela", "Thiago", "Vitória"]
+
+let nome = "catiucia"
+let achou = false
+
+for (i = 0; i < nomesComuns.length; i++) {
+    
+    if (nome == nomesComuns[i]) {
+        achou = true
+        break
+    }
+}
+
+if (achou) {
+    console.log("É, nome comum :P")
+} else {
+    console.log("Diferentão, hein? XD")
+}
